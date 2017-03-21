a7fa3eb37dcda0a8a4e132260a56d67ca9160fe3
DoomAl <alvaromonferrer11@gmail.com>
2017-03-21 14:40:11 +0000

    Create Salida.java

---
+++ src/main/java/Salida/Salida.java
@@ -0,0 +1,14 @@
+package Salida;
+
+import Contactos.Cliente;
+import Datos.Direccion;
+import Datos.Factura;
+import Datos.Llamada;
+
+/**
+ * Created by doomed on 15/03/16.
+ */
+public interface Salida {
+
+    void salGeneral(String cadena);
+}
