d9f8fd8f727bedc1279e4900c307fc8fc32e1c53
DoomAl <alvaromonferrer11@gmail.com>
2017-03-21 14:26:29 +0000

    Create Controlador.java

---
+++ src/main/java/controlador/Controlador.java
@@ -0,0 +1,19 @@
+package Controlador;
+
+/**
+ * Created by doomed on 4/05/16.
+ */
+public interface Controlador {
+    void nuevoCliente();
+    void nuevaFactura();
+    void nuevaTarifa();
+    void nuevaLlamada();
+    void buscarCliente();
+    void buscarFactura();
+    void busacrLlamada();
+    void eliminaDatos();
+    void almacenaDatos();
+    void recuperaDatos();
+
+    void nuevaTarifaBase();
+}
